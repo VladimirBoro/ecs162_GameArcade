743b7b1bae6e8031a98a78d1b016b626e0eaf154
VladimirBoro <vborodianski19@gmail.com>
2024-04-29 08:35:05 -0700

    main stuff

---
+++ src.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const snakeButtons = document.querySelectorAll(".snake");
+    snakeButtons.forEach((button) => { 
+        button.addEventListener("click", function() {window.location.href = "snake/snake.html";}); 
+    });
+
+    const pongButton = document.getElementById("pong");
+    pongButton.addEventListener("click", function() {window.location.href = "pong/pong.html";});
+    // pongButtons.forEach((button) => { 
+    //     button.addEventListener("click", function() {window.location.href = "pong/index.html";}); 
+    // });
+});
--- src2.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const startButtons = document.querySelectorAll(".snake");
-    startButtons.forEach((button) => { 
-        button.addEventListener("click", function() {window.location.href = "snake/snake.html";}); 
-    });
-});
